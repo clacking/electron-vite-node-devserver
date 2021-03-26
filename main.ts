cadfb82b56c26c2736d1a52381257cde1750c0d9
clacking <click@clacking.net>
2021-03-27 01:50:27 +0900

    fix main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 import { app, BrowserWindow } from 'electron';
 import { createServer } from 'vite';
-import serve from 'electron-serve';
+import * as serve from 'electron-serve';
 
 const DEV_SERVER_PORT = 5555;
 const DEV_SERVER = process.env.APP_ENV !== 'production';
 let mainWindow: BrowserWindow;
-const serveURL = serve({directory: './'});
+serve({directory: './'});
 
 async function main () {
     await app.whenReady();
@@ -23,7 +23,6 @@ async function main () {
         await devServer.listen();
         webview_page = `http://localhost:${DEV_SERVER_PORT}`;
     } else {
-        await serveURL(mainWindow);
         webview_page = `app://-`;
     }
     await mainWindow.loadURL(webview_page);
